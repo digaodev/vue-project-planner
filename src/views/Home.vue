649828ed1710bf0f0e8fe6904f26d4ad23676049
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-11 16:07:19 -0300

    feat: add Home component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,52 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <ul v-if="projects.length">
+      <li class="list-item" v-for="project in projects" :key="project.id">
+        <ProjectItem
+          :project="project"
+          @delete="handleDelete"
+          @toggleCompleted="handleToggleCompleted"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import ProjectItem from "../components/ProjectItem.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: { ProjectItem },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+  methods: {
+    handleDelete(projectId) {
+      this.projects = this.projects.filter((p) => p.id !== projectId);
+    },
+    handleToggleCompleted(projectId) {
+      this.projects = this.projects.map((p) => {
+        if (p.id === projectId) {
+          return {...p, completed: !p.completed}
+        }
+        return p
+      });
+    },
+  },
 };
 </script>
+
+<style scoped>
+.list-item {
+  list-style-type: none;
+}
+</style>
